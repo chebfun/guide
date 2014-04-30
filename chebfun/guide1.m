b5967b71570122c285cc95c35ce99ac0b3f484ab
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2014-04-30 18:43:14 +0100

    Many changes to guide1.m, though not fully polished yet.

--- chebfun/guide1.m
+++ chebfun/guide1.m
@@ -1,12 +1,12 @@
 %% CHEBFUN GUIDE 1: GETTING STARTED WITH CHEBFUN
-% Lloyd N. Trefethen, October 2009, latest revision October 2013
+% Lloyd N. Trefethen, October 2009, latest revision May 2014
 
 %% 1.1  What is a chebfun?
-% A chebfun is a function of one variable defined on an interval [a,b]. The
+% A chebfun is a function of one variable defined on an interval $[a,b]$. The
 % syntax for chebfuns is almost exactly the same as the usual Matlab syntax
 % for vectors, with the familiar Matlab commands for vectors overloaded in
-% natural ways. Thus, for example, whereas sum(f) returns the sum of the
-% entries when f is a vector, it returns a definite integral when f is a
+% natural ways. Thus, for example, whereas `sum(f)` returns the sum of the
+% entries when `f` is a vector, it returns a definite integral when `f` is a
 % chebfun.
 
 %%
@@ -29,7 +29,9 @@
 % 1,000,000 points. Chebfun makes use of adaptive procedures that aim to
 % find the right number of points automatically so as to represent each
 % function to roughly machine precision (about 15 digits of relative
-% accuracy).
+% accuracy).  (Originally Chebfun stored function values at Chebyshev
+% points; in Version 5 it switched to storing Chebyshev expansion 
+% coefficients.)
 
 %%
 % The mathematical foundations of Chebfun are for the most part well
@@ -45,9 +47,13 @@
 % Ricardo Pachon (from 2006), Rodrigo Platte (from 2007), and Toby Driscoll
 % and Nick Hale (from 2008). In 2009, Asgeir Birkisson and Mark Richardson
 % also became involved, and other contributors included Pedro Gonnet, Joris
-% Van Deun, and Georges Klein. The Chebfun Version 5 team includes Anthony
-% Austin, Asgeir Birkisson, Toby Driscoll, Nick Hale, Hrothgar, Mohsin
-% Javed, Hadrien Montanelli, Nick Trefethen, and Kuan Xu.
+% Van Deun, and Georges Klein.  Nick Hale served as Director of the
+% project during 2010-2014.  The Chebfun Version 5 rewrite was directed
+% by Nick Hale during 2013-2014 and the team included Anthony
+% Austin, Asgeir Birkisson, Toby Driscoll, Hrothgar, Mohsin
+% Javed, Hadrien Montanelli, Nick Trefethen, Grady Wright, and Kuan Xu.
+% Further information about Chebfun history is available at the Chebfun
+% web site.
 
 %%
 % This Guide is based on Chebfun Version 5, released
@@ -56,16 +62,16 @@
 % for Version 4 can found there.
 
 %% 1.2  Constructing simple chebfuns
-% The "chebfun" command constructs a chebfun from a specification such as a
+% The `chebfun` command constructs a chebfun from a specification such as a
 % string or an anonymous function.  If you don't specify an interval, then
-% the default interval [-1,1] is used. For example, the following command
-% makes a chebfun corresponding to cos(20x) on [-1,1] and plots it.
+% the default interval $[-1,1]$ is used. For example, the following command
+% makes a chebfun corresponding to $\cos(20x)$ on $[-1,1]$ and plots it.
   f = chebfun('cos(20*x)');
   plot(f)
 
 %%
-% From this little experiment, you cannot see that f is represented by a
-% polynomial.  One way to see this is to find the length of f:
+% From this little experiment, you cannot see that `f` is represented by a
+% polynomial.  One way to see this is to find the length of `f`:
   length(f)
 
 %%
@@ -73,21 +79,21 @@
   f
 
 %%
-% These results tell us that f is represented by a polynomial interpolant
+% These results tell us that `f` is represented by a polynomial interpolant
 % through 59 Chebyshev points, i.e., a polynomial of degree 58.  These
 % numbers have been determined by an adaptive process.  We can see the data
-% points by plotting f with the '.-' option:
+% points by plotting `f`with the `'.-'` option:
   plot(f,'.-')
   
 %%
-% The formula for N+1 Chebyshev points in [-1,1] is
+% The formula for $N+1$ Chebyshev points in $[-1,1]$ is
 %
-%           x(j) = -cos(j pi/N) ,     j = 0:N,
+% $$ x(j) = -\cos(j \pi/N), \quad  j = 0:N, $$
 %
 % and in the figure we can see that the points are clustered accordingly
-% near 1 and -1. Note that in the middle of the grid, there are about 5
+% near $1$ and $-1$. Note that in the middle of the grid, there are about 5
 % points per wavelength, which is evidently what it takes to represent this
-% cosine to 15 digits of accuracy.  For intervals other than [-1,1],
+% cosine to 15 digits of accuracy.  For intervals other than $[-1,1]$,
 % appropriate Chebyshev points are obtained by a linear scaling.
 
 %%
@@ -98,7 +104,7 @@
 % millions [Higham 2004].
 
 %%
-% What is the integral of f from -1 to 1?  Here it is:
+% What is the integral of $f$ from $-1$ to $1$?  Here it is:
   sum(f)
 
 %%
@@ -110,7 +116,7 @@
 %%
 % Here is another example, now with the chebfun defined by an anonymous
 % function instead of a string. In this case the interval is specified as
-% [0,100].
+% $[0,100]$.
   g = chebfun(@(t) besselj(0,t),[0,100]);
   plot(g), ylim([-.5 1])
 
@@ -121,7 +127,7 @@
 
 %%
 % Is it accurate?  Well, here are three random points
-% in [0,100]:
+% in $[0,100]$:
   format long
   x = 100*rand(3,1)
 
@@ -147,7 +153,7 @@
 %%
 % Most often we get a chebfun by operating on other chebfuns. For example,
 % here is a sequence that uses plus, times, divide, and power operations on
-% an initial chebfun "x" to produce a famous function of Runge:
+% an initial chebfun `x` to produce a famous function of Runge:
   x = chebfun('x');
   f = 1./(1+25*x.^2);
   length(f)
@@ -155,18 +161,18 @@
 
 %% 1.3  Operations on chebfuns
 % There are more than 200 commands that can be applied to
-% a chebfun.  For a list of many of them you can type "methods":
+% a chebfun.  For a list of many of them you can type `methods`:
   methods chebfun
 
 %%
-% To find out what a command does, you can use "help".
+% To find out what a command does, you can use `help`.
   help chebfun/chebpoly
 
 %%
 % Most of the commands in the list exist in ordinary Matlab; some
-% exceptions are "domain", "restrict", "chebpoly", "define", and "remez".
-% We have already seen "length" and "sum" in action.  In fact we have
-% already seen "subsref" too, since that is the Matlab command for (among
+% exceptions are `domain`, `restrict`, `chebpoly`, `define`, and `remez`.
+% We have already seen `length` and `sum` in action.  In fact we have
+% already seen `subsref` too, since that is the Matlab command for (among
 % other things) evaluating arguments in parentheses.
 %  Here is another example of its use:
   f(0.5)
@@ -176,8 +182,8 @@
   1/(1+25/4)
 
 %% 
-% In this Runge function example, we have also implicitly seen "times",
-% "plus", "power", and "rdivide", all of which have been overloaded from
+% In this Runge function example, we have also implicitly seen `times`,
+% `plus`, `power`, and `rdivide`, all of which have been overloaded from
 % their usual Matlab uses to apply to chebfuns.
 
 %%
@@ -192,7 +198,7 @@
 % "fun".  This enhancement of Chebfun was developed initially by Ricardo
 % Pachon during 2006-2007, then also by Rodrigo Platte starting in 2007
 % [Pachon, Platte and Trefethen 2009]. Essentially funs are the "classic
-% chebfuns" for smooth functions on [-1,1] originally implemented by
+% chebfuns" for smooth functions on $[-1,1]$ originally implemented by
 % Zachary Battles in Chebfun Version 1.
 
 %%
@@ -208,60 +214,60 @@
   f = chebfun('abs(x-.3)','splitting','on');
 
 %%
-% The "length" command reveals that f is defined by four data points,
-% namely two for each linear interval:
+% The `length` command reveals that `f` is defined by four data points,
+% two for each linear interval:
   length(f)
 
 %%
-% We can see the structure of f in more detail by typing f without a
+% We can see the structure of `f` in more detail by typing `f` without a
 % semicolon:
   f
 
 %%
 % This output confirms that f consists of two funs, each defined by two
 % points and two corresponding function values. We can see the structure
-% from another angle with disp:
+% from another angle with `disp`:
   disp(f)
   
 %%
-% This output again show that f consists of two funs with breakpoints at
-% -1, 1, and a snumber very close to 0.3.  The "Vscale" field is related to
-% the maximum absolute value of f and "Epslevel" gives some information (to
+% This output again show that `f` consists of two funs with breakpoints at
+% $-1$, $1$, and a number very close to $0.3$.  The `Vscale` field is related to
+% the maximum absolute value of `f` and `Epslevel` gives some information (to
 % be discussed later) about its relative accuracy.
 
 %%
 % Another way to make a piecewise smooth chebfun is to construct it
 % explicitly from various pieces.  For example, the following command
-% specifies three functions x^2, 1, and 4-x, together with a vector of
-% endpoints indicating that the first function applies on [-1,1], the
-% second on [1,2], and the third on [2,4]:
+% specifies three functions $x^2$, $1$, and $4-x$, together with a vector of
+% endpoints indicating that the first function applies on $[-1,1]$, the
+% second on $[1,2]$, and the third on $[2,4]$:
   f = chebfun({@(x) x.^2, @(x) 1+0*x, @(x) 4-x},[-1 1 2 4]);
   plot(f)
 
 %%
-% We expect f to consist of three pieces of lengths 3, 1, and 2, and this
+% We expect `f` to consist of three pieces of lengths 3, 1, and 2, and this
 % is indeed the case:
   f
 
 %%
-% Our eyes see pieces, but to Chebfun, f is just another function.  For
+% Our eyes see pieces, but to Chebfun, `f` is just another function.  For
 % example, here is its integral.
   sum(f)
 
 %%
-% Here is an algebraic transformation of f, which we plot in another color
+% Here is an algebraic transformation of `f`, which we plot in another color
 % for variety.
   plot(1./(1+f),'r')
 
 %% 
 % Some Chebfun commands naturally introduce breakpoints in a chebfun. For
-% example, the "abs" command first finds zeros of a function and introduces
+% example, the `abs` command first finds zeros of a function and introduces
 % breakpoints there.  Here is a chebfun consisting of 6 funs:
   f = abs(exp(x).*sin(8*x));
   plot(f)
 
 %%
-% And here is an example where breakpoints are introduced by the "max"
+% And here is an example where breakpoints are introduced by the `max`
 % command, leading to a chebfun with 13 pieces:
   f = sin(20*x);
   g = exp(x-1);
@@ -287,7 +293,7 @@
 % detection or "splitting" feature, when it is turned on, may subdivide
 % functions even though they do not have clean point singularities, and
 % this may be desirable or undesirable depending on the application.  For
-% example, considering sin(x) over [0,1000] with splitting on, we end up
+% example, considering $\sin(x)$ over $[0,1000]$ with splitting on, we end up
 % with a chebfun with many pieces:
   tic, f = chebfun('sin(x)',[0 1000*pi],'splitting','on'); toc
   disp(f)
@@ -298,11 +304,6 @@
 
   tic, f2 = chebfun('sin(x)',[0 1000*pi]); toc
   disp(f2)
-
-%%
-% In a Chebfun computation, splitting can be turned on and off freely to
-% handle different functions appropriately.  The default or "factory" value
-% is splitting off; see Chapter 8.
 
 %% 1.5  Infinite intervals and infinite function values
 % A major change from Chebfun Version 2 to Version 3 was the generalization of
@@ -328,7 +329,7 @@
 
 %%
 % Here is an example of a function that diverges to infinity,
-% which we can capture by including the flag 'blowup 2' (try help blowup
+% which we can capture by including the flag `'blowup 2'` (try help chebfun
 % for details):
   h = chebfun('(1/pi)./sqrt(1-x.^2)','blowup',2);
   plot(h)
@@ -361,37 +362,39 @@
   A'*A
 
 %%
-% These are called "quasimatrices", and they are discussed in
+% These are called _quasimatrices_, and they are discussed in
 % Chapter 6.
 
 %% 1.7  How this Guide is produced
-% This guide is produced in Matlab using the "publish" command. The
-% formatting is rather simple, not relying on TeX features or other fine
-% points of typesetting.  To publish a chapter for yourself, make sure the
+% This guide is produced in Matlab using the "publish" command with a 
+% style sheet somewhat different from the usual; the output of publish
+% is then processed by markdown.
+% To publish a chapter for yourself, make sure the
 % chebfun guide directory is in your path and then type, for example,
-% "open(publish('guide1'))".
+% `open(publish('guide1'))`.  The formatting may not be exactly 
+% right but it should certainly be legible.
 
 %% 1.8  References
 % [Battles & Trefethen 2004] Z. Battles and L. N. Trefethen, "An extension
-% of Matlab to continuous functions and operators", SIAM Journal on
-% Scientific Computing 25 (2004), 1743-1770.
+% of Matlab to continuous functions and operators", _SIAM Journal on
+% Scientific Computing_ 25 (2004), 1743-1770.
 % 
 % [Berrut & Trefethen 2005] J.-P. Berrut and L. N. Trefethen, "Barycentric
-% Lagrange interpolation", SIAM Review 46 (2004), 501-517.
+% Lagrange interpolation", _SIAM Review 46_ (2004), 501-517.
 %
 % [Higham 2004] N. J. Higham, "The numerical stability of barycentric
-% Lagrange interpolation", IMA Journal of Numerical Analysis 24 (2004),
+% Lagrange interpolation", _IMA Journal of Numerical Analysis_ 24 (2004),
 % 547-556.
 %
 % [Pachon, Platte & Trefethen 2009] R. Pachon, R. B. Platte and L. N.
-% Trefethen, "Piecewise smooth chebfuns", IMA J. Numer. Anal., 2009.
+% Trefethen, "Piecewise smooth chebfuns", _IMA J. Numer. Anal._, 2009.
 %
 % [Salzer 1972] H. E. Salzer, "Lagrangian interpolation at the Chebyshev
-% points cos(nu pi/n), nu = 0(1)n; some unnoted advantages", Computer
-% Journal 15 (1972), 156-159.
+% points cos(nu pi/n), nu = 0(1)n; some unnoted advantages", _Computer
+% Journal_ 15 (1972), 156-159.
 %
 % [Trefethen 2007] L. N. Trefethen, "Computing numerically with functions
-% instead of numbers", Mathematics in Computer Science 1 (2007), 9-19.
+% instead of numbers", _Mathematics in Computer Science_ 1 (2007), 9-19.
 %
-% [Trefethen 2013] L. N. Trefethen, Approximation Theory and
-% Approximation Practice, SIAM, 2013.
+% [Trefethen 2013] L. N. Trefethen, _Approximation Theory and
+% Approximation Practice_, SIAM, 2013.
