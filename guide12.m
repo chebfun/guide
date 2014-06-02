3dcec34bcbdc0307684b19099980568028908173
Hadrien Montanelli <montanelli@maths.ox.ac.uk>
2014-06-02 17:21:31 +0100

    1. Modifying numerotation of sections (12.X instead of 2.X) and references to other sections.
    2. Minor modifications of coding style.

--- guide12.m
+++ guide12.m
@@ -1,7 +1,7 @@
 %% 12. Chebfun2: Integration and Differentiation
 % Alex Townsend, March 2013, last updated May 2014
  
-%% 2.1 `sum` and `sum2`
+%% 12.1 `sum` and `sum2`
 % We have already seen the `sum2` command, which returns the definite double
 % integral of a chebfun2 over its domain of definition. The sum command 
 % is a little different and integrates with respect to one variable at a time. For
@@ -12,7 +12,7 @@ sum(f)
 %%
 % A chebfun is returned because the result depends on $x$ and hence, is a 
 % function of one variable.  Similarly, we can integrate over the
-% $x$ variable, and we plot the result. 
+% $x$ variable, and plot the result. 
 LW = 'linewidth';
 sum(f,2), plot(sum(f,2),LW,1.6) 
 
@@ -47,17 +47,17 @@ fprintf('CHEBFUN2/SUM2:  I = %17.15f  time = %6.4f secs\n',I,t)
 % quadrature by Carvajal, Chapman, and Geddes [Carvajal, Chapman & Geddes
 % 2005].
 
-%% 2.2 `norm', `mean', and `mean2`
+%% 12.2 `norm`, `mean`, and `mean2`
 % The $L^2$-norm of a function $f(x,y)$ can be computed as the square root of the 
 % double integral of $f^2$. In Chebfun2 the command `norm(f)`, 
 % without any additional arguments, computes this quantity. For example, 
-f = chebfun2( 'exp(-(x.^2 + y.^2 +4*x.*y))' );
+f = chebfun2('exp(-(x.^2 + y.^2 +4*x.*y))');
 norm(f), sqrt(sum2(f.^2))
 
 %%
 % Here is another example. This time we compute the norms of $f(x,y)$, 
 % $\cos(f(x,y))$, and $f(x,y)^5$.
-f = chebfun2( @(x,y) exp(-1./( sin(x.*y) + x ).^2) );
+f = chebfun2(@(x,y) exp(-1./( sin(x.*y) + x ).^2));
 norm(f), norm( cos(f) ), norm( f.^5 )
 %% 
 % Just as `sum2` performs double integration, `mean2` computes the 
@@ -82,7 +82,7 @@ title('Mean value of 2D Runge function wrt y')
 % $x$ variable, we obtain the mean value over the whole domain.
 mean(mean(runge))      % compare with mean2(runge)
 
-%% 2.3 `cumsum` and `cumsum2`
+%% 12.3 `cumsum` and `cumsum2`
 % The command `cumsum2` computes the double indefinite integral, which is a
 % function of two variables, and returns a chebfun2. 
 help chebfun2/cumsum2
@@ -98,7 +98,7 @@ contour(cumsum2(f),'numpts',400), axis equal
 title('Contours of cumsum2(f)'), axis([-1 1 -1 1])
 norm( cumsum(cumsum(f),2) - cumsum2(f) ) 
 
-%% 2.4 Complex encoding
+%% 12.4 Complex encoding
 % As is well known, a pair of real scalar functions $f$ and $g$ can be 
 % encoded as a complex function $f+ig$. This trick can be useful
 % for simplifying many operations, but at the same time may be confusing.
@@ -119,16 +119,16 @@ axis equal, title('One complex-valued function')
 % This complex encoding trick is used in a number of places in 
 % Chebfun2. Specifically, it's used to encode the path of integration 
 % for a line integral
-% (see section 2.5, below), to represent zero contours of a chebfun2 
-% (see Chapter 3), and to represent trajectories in vector 
-% fields (see Chapter 4). 
+% (see section 12.5, below), to represent zero contours of a chebfun2 
+% (see Chapter 13), and to represent trajectories in vector 
+% fields (see Chapter 14). 
 
 %%
 % We hope users become comfortable with using complex encodings, though 
 % they are not required for the majority of Chebfun2
 % functionality. 
 
-%% 2.5 Integration along curves
+%% 12.5 Integration along curves
 % Chebfun2 can compute the integral of $f(x,y)$ along a curve $(x(t),y(t))$. 
 % It uses the complex encoding trick and encode the curve $(x(t),y(t))$ 
 % as a complex valued chebfun $x(t) + iy(t)$.
@@ -143,7 +143,7 @@ plot3(real(C),imag(C),f(C),'k','linewidth',2)
 % We can compute this by restricting $f$ to the curve and then integrating
 sum(f(C))
 
-%% 2.6 `diff`
+%% 12.6 `diff`
 % In MATLAB the `diff` command calculates finite differences of a matrix 
 % along its columns (by default) or rows. For a chebfun2 the same syntax 
 % represents partial differentiation $\partial f/\partial y$ (by default) or 
@@ -159,7 +159,7 @@ u = real(f); v = imag(f);           % real and imaginary parts
 norm(diff(u) - (-diff(v,1,2)))      
 norm(diff(u,1,2) - diff(v))         % Do the Cauchy-Riemann eqns hold?
 
-%% 2.7 Integration in three variables 
+%% 12.7 Integration in three variables 
 % Chebfun2 also works pretty well for integration in three variables.
 % The idea is to integrate over two of the variables using Chebfun2 and the
 % remaining variable using Chebfun. We have selected a tolerance of $10^{-6}$
@@ -175,7 +175,7 @@ fprintf(' Chebfun2:  I = %16.14f  time = %5.3f secs\n',I,t)
 tic, I = integral3(f,-2,2,.5,2.5,1,2); t = toc;         % compare with MATLAB
 fprintf('Integral3:  I = %16.14f  time = %5.3f secs\n',I,t)
 
-%% 2.9 References
+%% 12.8 References
 %
 % [Carvajal, Chapman & Geddes 2005] O. A. Carvajal, F. W. Chapman and 
 % K. O. Geddes, Hybrid symbolic-numeric integration in multiple dimensions 
