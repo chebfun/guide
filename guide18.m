61ecb382ef1ecd384d346e253f489f35d7327c63
Behnam Hashemi <hashemi@maths.ox.ac.uk>
2016-03-09 13:45:54 +0000

    update Chapter 18

--- guide18.m
+++ guide18.m
@@ -1,9 +1,14 @@
 %% 18. Chebfun3
 % Behnam Hashemi and Nick Trefethen, November 2015
 
+%%
+% NOTE: Chebfun3 is not yet publicly available,
+% but we expect to release it during 2016.  Some of
+% the details described below may change.
+
 %% 18.1  Introduction
 % The Chebfun project began in 2003, and Chebfun2, for 2D functions, was first
-% released in 2013.  Chebfun3, for 3D functions, has been created by Behnam
+% released in 2013. Chebfun3, for 3D functions, has been created by Behnam
 % Hashemi and will be released sometime in 2016.
 
 %%
@@ -96,7 +101,7 @@ clf, isosurface(f)
 % are positive integers whose size might typically be 10 or 20 for
 % the functions that Chebfun3 can efficiently represent.  These
 % three quasimatrices are combined in a product with coefficients
-% specified by a $r_1\times r_2\times r_3$ _core tensor_ called `core`.  
+% specified by an $r_1\times r_2\times r_3$ _core tensor_ called `core`.  
 % In short, we write:
 % $$ f(x,y,z) \approx `core` \times_1 c(x) \times_2 r(y) \times_3
 % t(z), $$ or more fully:
@@ -171,7 +176,7 @@ plotcoeffs(f,'.-')
 % Of course, Chebfun is all about computing with functions, not
 % just representing them.  For example, here are two 3D functions:
 f = chebfun3(@(x,y,z) sin(x+y.*z));
-g = chebfun3(@(x,y,z) cos(15*exp(z))./(5+x+2*y+z));
+g = chebfun3(@(x,y,z) cos(15*exp(z))./(5+x.^3+2*y.^2+z));
 
 %%
 % Here are the maxima of $f$, $g$, and $fg$:
@@ -214,7 +219,7 @@ exact = -sqrt(1+(8*pi)^2)/(8*pi)
 % We can examine its columns, rows, and tubes by executing
 % `f.cols`, `f.rows`, and `f.tubes`.  For example, let us look at the
 % columns associated with the chebfun3 $g$ just considered.
-% This is a quasimatrix with 11 columns:
+% This is a quasimatrix with 9 columns:
 size(g.cols)
 
 %%
@@ -227,15 +232,15 @@ plot(g.tubes)
 
 %%
 % A plot of _all_ the coefficients of $g$ looks like this.
-plotcoeffs(g,'-.')
+plotcoeffs(g,'.-')
 
 %%
 % If we look just at the columns we get the corresponding piece:
-clf, plotcoeffs(g.cols,'-.')
+clf, plotcoeffs(g.cols,'.-')
 
 %% 18.5  Periodic chebfun3 objects 
 % Chebfun3 can use trigonometric functions instead of polynomials for 
-% representing smooth functions which are periodic in all three variables. 
+% representing smooth functions which are triply periodic. 
 % To create a trig-based chebfun3 object, we can use the 'trig' (or 'periodic') 
 % flag in the Chebfun3 constructor. For example, the function 
 % $f(x,y,z) = \tanh(3\sin x) - \sin(y+1/2) + \cos(6z)$ on $[-\pi, \pi)^3$ 
@@ -250,13 +255,13 @@ f.cols
 %% 
 % Here is the length of $f$ and a plot of its coefficients:
 [m, n, p] = length(f)
-plotcoeffs(f,'-.')
+plotcoeffs(f,'.-')
 
 %%
 % As we see, $f$ is resolved to machine precision using trigonometric 
-% interpolants through 143, 3, and 13 equally spaced samples over $[-\pi,\pi)^3$
+% interpolants through 143, 5, and 13 equally spaced samples over $[-\pi,\pi)^3$
 % in the $x$, $y$, and $z$ directions, respectively. In other words,
-% trigonometric polynomials of degrees 71, 1, and 6 are enough to resolve 
+% trigonometric polynomials of degrees 71, 2, and 6 are enough to resolve 
 % this function. Let's compare with the length of $f$ if we ignore its periodicity:
 fCheb = chebfun3(@(x,y,z) tanh(3*sin(x))-sin(2*y+1/2)+cos(6*z), [-pi pi -pi pi -pi pi]);
 [m_fCheb, n_fCheb, p_fCheb] = length(fCheb)
@@ -272,17 +277,13 @@ fCheb = chebfun3(@(x,y,z) tanh(3*sin(x))-sin(2*y+1/2)+cos(6*z), [-pi pi -pi pi -
 % can take appropriate arguments to specify dimensions.
 
 %%
-% For example, the following function is equal to $x^2$ plus a harmonic function:
-f = chebfun3(@(x,y,z) x.^2 + 1./sqrt(x.^2 + y.^2 + (2-z).^2))
-
-%%
-% [Remark to Chebfunners: a harmonic function gives scaling trouble... to be discussed.]
+% For example, the following function is a harmonic function:
+f = chebfun3(@(x,y,z) 1./sqrt(x.^2 + y.^2 + (2-z).^2));
 
 %%
 % So its Laplacian will be the constant 2:
 Lf = lap(f);
 Lf(rand(3,1),rand(3,1),rand(3,1))
-
 %%
 % Let's compare the Laplacian of $f$ with the divergence of its gradient,
 % which will be discussed in the next subsection.
@@ -318,8 +319,9 @@ view([2 2 40])
 % along a smooth curve depends only on the endpoints of the path.
 f = chebfun3(@(x,y,z) sin(x+20*y+z.^2).*exp(-(3+y.^2)) , [-5*pi, 5*pi, -5*pi, 5*pi, -5*pi, 5*pi]);
 F = grad(f);
-curve = chebfun(@(t) [t.*cos(t) t.*sin(t) t], [0, 5*pi]); % Conical spiral
-plot3(curve(:,1), curve(:,2), curve(:,3),'b'), shg
+curve = chebfun(@(t) [t.*cos(t) t.*sin(t) t], [0, 5*pi]); 
+plot3(curve(:,1), curve(:,2), curve(:,3),'b'), 
+title('Conical spiral',FS,fs), shg
 I_spiral = integral(F,curve)
 ends = f(5*pi*cos(5*pi), 5*pi*sin(5*pi), 5*pi) - f(0, 0, 0)
 
@@ -333,9 +335,14 @@ norm( curl(F) )
 % integral of our vector field over two paths with the same endpoints:
 curve2 = chebfun(@(t) [t*cos(5*pi) t*sin(5*pi) t], [0, 5*pi]);
 hold on, plot3(curve2(:,1), curve2(:,2), curve2(:,3),'r-.'), hold off, shg
-I_line = integral(F,curve2); I_line - I_spiral
+I_line = integral(F,curve2), I_line - I_spiral
 
 %% 18.8 Rootfinding
+% Rootfinding in Chebfun3 is still under development.  What follows
+% describes the code `root`, which attempts to find just one single root
+% of a chebfun3v or equivalently of three chebfun3 objects.
+
+%%
 % One usually expects three 3D functions to have a finite number of common
 % roots, though it is possible that such a system of equations would have an 
 % infinite number of roots. Given $f, g$, and $h$, rootfinding in Chebfun3
@@ -410,7 +417,110 @@ norm(squeeze(Score(:,1,:).*Score(:,2,:)))
 % And also its horizontal slices are orthogonal:
 norm(squeeze(Score(1,:,:).*Score(2,:,:)))
 
-%% 18.10 References 
+%% 18.10 Changing the accuracy with chebfun3eps
+% Chebfun has always had a parameter that describes its
+% target accuracy, traditionally named `eps` though in the
+% process of being renamed to `chebfuneps`.  For 1D computations,
+% we do not recommend that users normally change this parameter
+% from its factory value of machine precision (unless dealing with
+% noisy functions), because the speedups
+% to be obtained are usually not very large.  See section
+% 8.8 of this Guide.
+
+%%
+% In two dimensions, and even more in three dimensions, the
+% potential gains from loosening the tolerance become much
+% greater.  Many users of Chebfun3 may find, for example, that
+% they want to work with 10 digits of accuracy rather than 16 --
+% the speedup in many cases is on the order of a factor of 10.
+% For this reason, as of very recently, Chebfun allows users
+% to set different tolerances `chebfuneps`, `chebfun2eps`, and
+% `chebfun3eps` for computations in 1D, 2D and 3D.  At this moment
+% of writing -- though it may change! -- the factory values of
+% these parameters are all machine epsilon.
+
+%%
+% To illustrate the speedups, here we compute chebfun3 objects
+% for a Runge function at four different accuracies.  We caution
+% users that the actual accuracy achieved may be a digit or two
+% less than requested.
+
+disp('   eps     time      rank m-length n-length p-length')
+ff = @(x,y,z)1./(0.01+x.^2+y.^2+z.^2);
+for ep = 10.^(-16:4:-4)
+  f = chebfun3(ff,'eps', ep);
+  tic, f = chebfun3(ff,'eps', ep); t = toc;
+  r = rank(f);
+  [m,n,p] = length(f);
+  fprintf('%8.1e  %6.4f %7d %7d %7d %7d\n', ep,t,r,m,n,p)
+end 
+
+%%
+% As the above example indicates, one way to control the
+% accuracy of a chebfun3 construction is by explicitly specifying
+% `eps` in a call to the constructor.  Alternatively -- and
+% necessarily, if you are doing follow-on operations on previously
+% constructed chebfun3 objects -- you can change the parameter
+% globally as described in Chapter 8.  For example, let us check the 
+% speed and accuracy of a certain computation using the factory value of 
+% `chebfun3eps`, i.e., machine precision. It runs slowly, and gives high 
+% accuracy:
+ff = @(x,y,z) tanh(2*(x+y+z));
+
+tic
+f = chebfun3(ff);
+g = cos(f+1).^2;
+error = g(.5,.6,.7) - cos(ff(.5,.6,.7)+1)^2
+toc
+
+%% 
+% Now let us set `chebfun3eps` to 1e-10 and run the same computation.  
+% Faster and less accurate!
+chebfun3eps 1e-10
+
+tic
+f = chebfun3(ff);
+g = cos(f+1).^2;
+error = g(.5,.6,.7) - cos(ff(.5,.6,.7)+1)^2
+toc
+
+%% 
+% The following command reverts `chebfun3eps` to the factory value:
+chebfun3eps factory
+
+%% 18.11 Chebfun3t for pure tensor product comparisons
+% Chebfun3, like Chebfun2 before it, exploits low-rank compression
+% of functions where possible.  The question of how much one gains
+% from this on average is controversial and certainly unresolved.
+% One can construct examples where the gain is as large as you like,
+% and other examples where there is no gain at all (and indeed where
+% the low-rank algorithms take longer).
+
+%%
+% To enable interested users to explore these tradeoffs, Chebfun
+% offers a certain fraction of the Chebfun3 functionality implemented
+% in a completely different, more straightforward but not
+% rank-compressed fashion.  The command `chebfun3t` will construct
+% a chebfun3t object that is represented as a multivariate Chebyshev
+% polynomial defined by a tensor of Chebyshev coefficients.
+% Here is an example where Chebfun3 is much faster than Chebfun3t:
+ff = @(x,y,z) sin(120*(x+y+z));
+tic, chebfun3(ff), toc
+tic, chebfun3t(ff), toc
+
+%%
+% On the other hand here is an example where we get no benefit:
+ff = @(x,y,z) tanh(3*(x+y+z));
+tic, chebfun3(ff), toc
+tic, chebfun3t(ff), toc
+
+%%
+% Let us emphasize that the main tool we are offering for
+% computation with functions in 3D is Chebfun3, not Chebfun3t.
+% The latter, only partially implemented, as only provided to
+% enable certain comparisons.
+
+%% 18.12 References 
 % 
 % [Bebendorf 2011] M. Bebendorf, "Adaptive cross approximation of 
 % multivariate functions", _Constructive Approximation_, 34 (2011) 149-179.
@@ -429,8 +539,8 @@ norm(squeeze(Score(1,:,:).*Score(2,:,:)))
 % and Y. M. Marzouk, "Function-train: a continuous analogue of the tensor-train 
 % decomposition", arXiv:1510.09088v1, 2015.
 %
-% [Hashemi & Trefethen 2016] B. Hashemi and L. N. Trefethen, manuscript in 
-% preparation.
+% [Hashemi & Trefethen 2016] B. Hashemi and L. N. Trefethen, "Chebfun in 
+% three dimensions", manuscript in preparation.
 %
 % [Townsend & Trefethen 2013a] A. Townsend and L. N. Trefethen, "Gaussian 
 % elimination as an iterative algorithm", _SIAM News_, 46, March 2013.
