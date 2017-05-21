d5a39567794cce0648e715ae129b5609fbfe3108
Hadrien Montanelli <montanelli@maths.ox.ac.uk>
2017-05-21 15:49:56 +0100

    Update guide19.

--- guide19.m
+++ guide19.m
@@ -1,4 +1,4 @@
-%% 19. SPIN, SPIN2 and SPIN3 for stiff PDEs
+%% 19. SPIN, SPIN2, SPIN3 and SPINSPHERE for stiff PDEs
 % H. Montanelli and L. N. Trefethen, February 2016, latest revision May
 % 2017
 
@@ -9,7 +9,7 @@
 % $$ u_t = S(u) = Lu + N(u) \quad\quad (1) $$
 %
 % where $L$ is a constant-coefficient linear differential operator on a domain
-% in one, two, or three space dimensions and $N$ is a constant-coefficient
+% in 1D/2D/3D or on the sphere, and $N$ is a constant-coefficient
 % nonlinear differential (or non-differential) operator of lower order on the 
 % same domain.  In applications, PDEs of this kind typically arise when two or 
 % more different physical processes are combined, and many PDEs of interest in
@@ -20,9 +20,9 @@
 % a single scalar equation is involved, for example in the Gray-Scott and 
 % Schnakenberg equations, which involve two components coupled together. (The 
 % importance of coupling of nonequal diffusion constants in science was made 
-% famous by Alan Turing in the most highly-cited of all his papers [12].) An 
+% famous by Alan Turing in the most highly-cited of all his papers [14].) An 
 % example of a third-order PDE is the KdV equation, the starting point 
-% of the study of nonlinear waves and solitons.  Fourth-order terms also arise, 
+% of the study of nonlinear waves and solitons. Fourth-order terms also arise, 
 % for example in the Cahn-Hilliard equation, whose solutions describe structures
 % of alloys, and the Kuramoto-Sivashinksy equation, related to combustion 
 % problems among others, whose solutions are chaotic.
@@ -33,26 +33,33 @@
 % that take advantage of special structure of the problem. One special feature 
 % is that the higher-order terms of the equation are linear, and another is that 
 % in many applications boundary effects are not the scientific issue, so that it 
-% suffices to consider periodic domains.
+% suffices to consider periodic domains (in 1D/2D/3D)---for the sphere,
+% periodicity naturally arises (see Chapter 17).
 
 %%
-% Specifically, the Chebfun codes `spin`/`spin2`/`spin3` we shall describe--
-% which stand for "stiff PDE integrator"--are based on the numerical methods 
-% known as _exponential integrators_. As we shall describe, these are very 
-% general codes that can employ an arbitrary solver in this class; by default, 
-% they use the fourth-order stiff time-stepper known as ETDRK4, devised by Cox 
-% and Matthews [4]. They can also handle entirely general problems of the form 
-% (1), but again convenient defaults are in place, an in particular, one can 
-% quickly solve well-known problems by specifying (case-insensitive) flags such 
-% as `burg`, `kdv` and `ks`.
+% Specifically, the Chebfun codes `spin`/`spin2`/`spin3`/`spinsphere` we shall 
+% describe--which stand for "stiff PDE integrator"--are based on _exponential 
+% integrators_ (in 1D/2D/3D) and _implicit-explicit schemes_ (on the sphere). 
+% As we shall describe, `spin`/`spin2`/`spin3` are very general codes that can 
+% employ an arbitrary exponential integrator; by default, they use the 
+% fourth-order stiff time-stepping scheme known as ETDRK4, devised by Cox and 
+% Matthews [5]. The `spinsphere` code uses the standard IMEX-BDF4 scheme for 
+% diffusive PDEs and the LIRK4 scheme [3] for dispersive PDEs. 
 
 %%
-% As we shall describe, `spin`/`spin2`/`spin3` take as inputs an initial 
-% function in the form of a `chebfun`, `chebfun2`, or `chebfun3` (with 
-% appropriate generalizations for systems, i.e., `chebmatrix` objects), compute 
-% over a specified time interval, and output another `chebfun`, `chebfun2` or 
-% `chebfun3` corresponding to the final time (and also intermediate times if 
-% requested). By default they show movies of the computation as it goes.
+% These codes handle entirely general problems of the form (1), but convenient 
+% defaults are in place, an in particular, one can quickly solve well-known 
+% problems by specifying (case-insensitive) flags such as `burg`, `kdv`, `gl` 
+% and `ks`.
+
+%%
+% As we shall describe, `spin`/`spin2`/`spin3`/`spinsphere` take as inputs an 
+% initial function in the form of a `chebfun`, `chebfun2`, `chebfun3` or 
+% `spherefun` (with appropriate generalizations for systems, i.e., `chebmatrix
+% objects), compute over a specified time interval, and output another `chebfun`, 
+% `chebfun2`, `chebfun3` or `spherefun` corresponding to the final time (and 
+% also intermediate times if requested). By default they show movies of the 
+% computation as it goes.
 
 %% 19.2 Computations in 1D with `spin`
 % The simplest way to see `spin` in action is to type simply `spin('ks')` or 
@@ -158,12 +165,16 @@ end
 % In 3D, the demo `spin3('gl3')` solves the PDE on $[0,100]^3$ and produces a 
 % movie to time $t=70$.
 
-%% 19.4 Using different exponential integrators and managing preferences with `spinpref`
-% The `spin`/`spin2`/`spin3` codes use the classes `spinpref`, `spinpref2` and
-% `spinpref3` to manage preferences, including the choice of the exponential 
-% integrator for the time-stepping, the value of the time-step, the number of 
-% grid points and various other options. See `help spinpref`/`spinpref2`/
-% `spinpref3` for complete lists of preferences. 
+%% 19.4 Computations on the sphere with `spinsphere`
+% [13] paper
+
+%% 19.5 Using different exponential integrators and managing preferences with `spinpref`
+% The `spin`/`spin2`/`spin3`/`spinsphere` codes use the classes `spinpref`, 
+% `spinpref2`, `spinpref3` and `spinprefsphere` to manage preferences, including 
+% the choice of the exponential integrator/implicit-explicit schemes for the 
+% time-stepping, the value of the time-step, the number of grid points and 
+% various other options. See `help spinpref`/`spinpref2`/`spinpref3`/`spinprefsphere` 
+% for complete lists of preferences. 
 
 %% 
 % For example, to solve the Kuramoto-Sivashinsky (KS) equation using the
@@ -186,19 +197,19 @@ u = spin2(S, 100, 2e-1, pref);
 % or simply
 u = spin2(S, 100, 2e-1, 'plot', 'off');
 
-%% 19.5 A quick note on history
+%% 19.6 A quick note on history
 % The history of exponential integrators for ODEs goes back at least to Hersch 
-% [5] and Certaine [3]. The extensive use of these formulas for solving stiff 
-% PDEs seems to have been initiated by the papers by Cox and Matthews [4], which 
+% [6] and Certaine [5]. The extensive use of these formulas for solving stiff 
+% PDEs seems to have been initiated by the papers by Cox and Matthews [5], which 
 % also introduced the ETDRK4 scheme that is the default used by `spin`, and
-% Kassam and Trefethen [8].  A striking unpublished paper by Kassam [7] shows 
+% Kassam and Trefethen [9].  A striking unpublished paper by Kassam [7] shows 
 % how effective such methods can be also for PDEs in 2D and 3D. A software 
 % package for such computations called EXPINT was produced by Berland, 
 % Skaflestad and Wright in 2007 [1]. A comprehensive theoretical understanding 
 % of these schemes has been presented by Hochbruck and Ostermann in a number
-% of papers, including [6]. The practical business of comparing many different 
-% schemes has been carried out by Minchev and Wright [9], then Bootland [2] and
-% Montanelli and Bootland [10]. Both of these latter projects were motivated by 
+% of papers, including [7]. The practical business of comparing many different 
+% schemes has been carried out by Minchev and Wright [11], then Bootland [2] and
+% Montanelli and Bootland [12]. Both of these latter projects were motivated by 
 % the challenge of choosing a good all-purpose integrator for use in Chebfun,
 % and the expectation was that a 5th- or 6th-order or even 7th-order integrator 
 % would be best; but in the end we have been unable to find a method that
@@ -213,35 +224,42 @@ u = spin2(S, 100, 2e-1, 'plot', 'off');
 % [2] N. J. Bootland, _Exponential integrators for stiff PDEs_, MSc thesis, 
 %     University of Oxford, 2014.
 %
-% [3] J. Certaine, _The solution of ordinary differential equations with large 
+% [3] M. P. Calvo, J. de Frutos and J. Novo, _Linearly implicit Runge--Kutta 
+%     methods for advection-reaction-diffusion equations_, Appl. Numer. Math.,
+%     37 (2001), pp. 535-549.
+% 
+% [4] J. Certaine, _The solution of ordinary differential equations with large 
 %     time constants_, in Mathematical methods for digital computers, Wiley,
 %     New York, 1960, pp. 128--132.
 %
-% [4] S. M. Cox and P. C. Matthews, _Exponential time differencing for stiff
+% [5] S. M. Cox and P. C. Matthews, _Exponential time differencing for stiff
 %     systems_, J. Comput. Phys. 176 (2002), pp. 430--455.
 %
-% [5] J. Hersch, _Contribution à la methode des equations aux differences_,
+% [6] J. Hersch, _Contribution à la methode des equations aux différences_,
 %     Z. Angew. Math. und Phys., 9 (1958), pp. 129--180. 
 %   
-% [6] M. Hochbruck and A. Ostermann, _Exponential integrators_, Acta Numerica,
+% [7] M. Hochbruck and A. Ostermann, _Exponential integrators_, Acta Numerica,
 %     19 (2010), pp. 209--286.
 %
-% [7] A.-K. Kassam, _Solving reaction-diffusion equations 10 times faster_, 
+% [8] A.-K. Kassam, _Solving reaction-diffusion equations 10 times faster_, 
 %     Tech. Rep. 1192, Numerical Analysis Group, University of Oxford, 2003.
 %
-% [8] A.-K. Kassam and L. N. Trefethen, _Fourth-order time-stepping for stiff
+% [9] A.-K. Kassam and L. N. Trefethen, _Fourth-order time-stepping for stiff
 %     PDEs_, SIAM J. Sci. Comp., 26 (2005), pp. 1214--1233.
 %
-% [9] V. T. Luan and A. Ostermann, _Explicit exponential Runge--Kutta methods of
+% [10] V. T. Luan and A. Ostermann, _Explicit exponential Runge--Kutta methods of
 %     high order for parabolic problems_, J. Comput. Appl. Math., 256 (2014),
 %     pp. 168-179.
 %
-% [10] B.V. Minchev and W. M. Wright, _A review of exponential integrators for 
+% [11] B.V. Minchev and W. M. Wright, _A review of exponential integrators for 
 %      first order semi-linear problems_, Tech. Rep. 2/2005, Norwegian 
 %      University of Science and Technology, 2005.
 %
-% [11] H. Montanelli and N. J. Bootland, _Solving periodic semilinear stiff PDEs 
+% [12] H. Montanelli and N. J. Bootland, _Solving periodic semilinear stiff PDEs 
 %      in 1D, 2D and 3D with exponential integrators_, submitted, 2016.
 %
-% [12] A. M. Turing, _The chemical basis of morphogenesis_, Phil. Trans.
+% [13] H. Montanelli and Y. Nakatsukasa, _Fourth-order time-stepping for PDEs on
+%      the sphere_, submitted, 2017.
+%
+% [14] A. M. Turing, _The chemical basis of morphogenesis_, Phil. Trans.
 %      Roy. Soc. Lond. B, 237 (1952), pp. 37--72.
