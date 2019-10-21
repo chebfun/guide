e52040548ad166ce48f49afbfdb715708b96e101
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2019-10-21 08:14:44 +0100

    updates to guide14

--- guide14.m
+++ guide14.m
@@ -1,42 +1,48 @@
 %% 14. Chebfun2: Rootfinding and Optimisation
-% Alex Townsend, March 2013, latest revision December 2014
+% Alex Townsend, March 2013, latest revision Octrober 2019
 
-%% 14.1 Zero contours 
-% Chebfun2 comes with the capability to compute the zero contours of a
-% function of two variables. For example, here we compute a representation
+%% 14.1 Zero contours of a bivariate function: |roots|
+% The Chebfun2 |roots| command can compute the zero contours of a
+% function of two variables. For example, here we compute the zero contours
+% of a representation
 % of Trott's curve, an example from algebraic geometry [Trott 1997].
-x = chebfun2(@(x,y) x); y = chebfun2(@(x,y) y); 
+cheb.xy;
 trott = 144*(x.^4+y.^4)-225*(x.^2+y.^2) + 350*x.^2.*y.^2+81;
 r = roots(trott);
-LW = 'linewidth'; MS = 'markersize';
-plot(r,LW,1.6), axis([-1 1 -1 1]), axis square
+plot(r), axis([-1 1 -1 1]), axis square
 
 %%
-% The zero curves are represented as complex-valued chebfuns
-% (see Section 13.4).   For example, 
+% The curves are represented as complex-valued chebfuns
+% (see Section 13.4).  For example, here is one of the four components:
 r(:,1)
 
 %%
-% The zero contours of a function are computed by Chebfun2 to plotting
-% accuracy: they are typically not accurate to machine precision. 
-
-%% 14.2 |roots|
-% Chebfun2 also comes with the capability of finding zeros of bivariate
-% systems, i.e., the solutions to $f(x,y) = g(x,y) = 0$. 
-% If the |roots| command is supplied with one chebfun2, it computes the zero
-% contours of that function, as in the last
-% section.  However, if it is supplied
-% with two chebfun2 objects, as in |roots(f,g)|, then it 
-% computes the roots of the bivariate system. Generically, these are
-% isolated points.
+% Although |roots| can be outwitted, it is often
+% remarkably accurate.  For example, here is
+% the perimeter of a circle of radius 1/2, which we measure by using the
+% fact that the arc length is equal to the 1-norm of the derivative.
+f = x.^2 + y.^2 - 1/4;
+perimeter = norm(diff(roots(f)),1)
+exact_perimeter = pi
 
 %%
-% What points on Trott's curve intersect the circle of radius $0.9$?
-g = chebfun2(@(x,y) x.^2 + y.^2 - .9^2);
+% For some more exotic examples of zero curves computed by
+% |roots|, see the 2D Approximation section of the Chebfun examples
+% collection at |www.chebfun.org|.
+
+%% 14.2 Zeros of a pair of bivariate functions: |roots| again
+% Chebfun2 |roots| can also find zeros of bivariate
+% systems, i.e., solutions to $f(x,y) = g(x,y) = 0$. 
+% Generically, these are isolated points.
+
+%%
+% For example, which points on Trott's curve intersect the circle of radius $0.9$?
+g = x.^2 + y.^2 - .9^2;
 r = roots(trott,g); 
-plot(roots(trott),'b',LW,1.6), hold on
-plot(roots(g),'r',LW,1.6)
-plot(r(:,1),r(:,2),'.k',LW,1.6,MS,20)
+plot(roots(trott),'b'), hold on
+plot(roots(g),'r')
+MS = 'markersize';
+plot(r(:,1),r(:,2),'.k',MS,20)
 axis([-1 1 -1 1]), axis square, hold off
 
 %%
@@ -47,13 +53,13 @@ axis([-1 1 -1 1]), axis square, hold off
 % The problem of determining the intersections
 % of real parameterised complex curves can be expressed as a
 % bivariate rootfinding problem.  For instance, here are the intersections
-% between the 'splat' curve [Guettel 2010] and a 'figure-of-eight'
+% between the 'splat' curve [G&uuml;ttel 2010] and a 'figure-of-eight'
 % curve. 
 t = chebfun('t',[0,2*pi]);
-sp = exp(1i*t) + (1+1i)*sin(6*t).^2;     % splat curve
+sp = exp(1i*t) + (1+1i)*sin(6*t)^2;      % splat curve
 figof8 = cos(t) + 1i*sin(2*t);           % figure of eight curve
-plot(sp,LW,1.6), hold on
-plot(figof8,'r',LW,1.6), axis equal
+plot(sp), hold on
+plot(figof8,'r'), axis equal
 
 d = [0 2*pi 0 2*pi];
 f = chebfun2(@(s,t) sp(t)-figof8(s),d);  % rootfinding
@@ -69,7 +75,7 @@ hold off
 %% 14.4 Global optimisation: |max2|, |min2|, and |minandmax2|
 % Chebfun2 also provides functionality for global optimisation. Here is
 % an example, where we plot the minimum and maximum as red dots.
-f = chebfun2(@(x,y) sin(30*x.*y) + sin(10*y.*x.^2) + exp(-x.^2-(y-.8).^2));
+f = sin(30*x.*y) + sin(10*y.*x.^2) + exp(-x.^2-(y-.8).^2);
 [mn mnloc] = min2(f); 
 [mx mxloc] = max2(f); 
 plot(f), hold on 
@@ -81,15 +87,15 @@ zlim([-6 6]), colormap('bone'), hold off
 % If both the global maximum and minimum are required, it is roughly twice
 % as fast to compute them at the same time by using the |minandmax2| command.
 % For instance, 
-tic; [mn mnloc] = min2(f);  [mx mxloc] = max2(f); t=toc;
+tic; [mn mnloc] = min2(f);  [mx mxloc] = max2(f); t = toc;
 fprintf('min2 and max2 separately = %5.3fs\n',t)
-tic; [Y X] = minandmax2(f); t=toc;
+tic; [Y X] = minandmax2(f); t = toc;
 fprintf('minandmax2 command = %5.3fs\n',t)
 
 %%
 % Here is a complicated function from the 2002 SIAM 100-Dollar,
-% 100-Digit Challenge.  Chebfun2 computes its global minimum
-% in a few seconds:
+% 100-Digit Challenge [Bornemann et al. 2004].  Chebfun2 computes its global minimum
+% in a fraction of a second:
 tic
 f = cheb.gallery2('challenge');
 [minval,minpos] = min2(f);
@@ -98,23 +104,23 @@ toc
 
 %%
 % The result closely matches the correct solution, computed
-% to 10,000 digits Bornemann et al. [2004]:
+% to 10,000 digits by Bornemann et al.:
 exact = -3.306868647475237280076113
 
 %%
 % Here is a contour plot of this wiggly function, with the
 % minimum circled in black: 
 colormap('default'), contour(f), hold on
-plot(minpos(1),minpos(2),'ok',MS,20,LW,3), hold off
+plot(minpos(1),minpos(2),'ok',MS,20), hold off
 
 %% 14.5 Critical points
 % The critical points of a smooth function of two variables can be located 
 % by finding the zeros of $\partial f/ \partial y = \partial f / \partial x = 0$.   
 % This is a rootfinding problem.  For example,
-f = chebfun2(@(x,y) (x.^2-y.^3+1/8).*sin(10*x.*y));
+f = (x.^2-y.^3+1/8).*sin(10*x.*y);
 r = roots(gradient(f));                       % critical points
-plot(roots(diff(f,1,2)),'b',LW,1.2), hold on  % zero contours of f_x
-plot(roots(diff(f)),'r',LW,1.2)               % zero contours of f_y
+plot(roots(diff(f,1,2)),'b'), hold on         % zero contours of f_x
+plot(roots(diff(f)),'r')                      % zero contours of f_y
 plot(r(:,1),r(:,2),'k.',MS,24)                % extrema
 axis([-1,1,-1,1]), axis square
 
@@ -129,22 +135,23 @@ axis([-1,1,-1,1]), axis square
 % The $\infty$-norm of a function is the maximum absolute value in its 
 % domain. It can be computed by passing the 
 % argument |inf| to the |norm| command.  
-f = chebfun2(@(x,y) sin(30*x.*y));
+f = sin(30*x.*y);
 norm(f,inf)
 
 %% 14.7 References
 %
 % [Bornemann et al. 2004] F. Bornemann, D. Laurie, S. Wagon and
 % J. Waldvogel, _The SIAM 100-Digit Challenge: A Study in
-% High-Accuracy Nuemrical Computing_, SIAM 2004.
+% High-Accuracy Numerical Computing_, SIAM 2004.
 %
-% [Guettel 2010] S. Guettel, 
+% [G&uuml;ttel 2010] S. G&uuml;ttel, 
 % "Area and centroid of a 2D region", 
 % http://www.chebfun.org/examples/geom/Area.html.
 % 
 % [Nakatsukasa, Noferini & Townsend 2014] Y. Nakatsukasa, V. Noferini
 % and A. Townsend, "Computing the common zeros of two bivariate functions
-% via Bezout resultants", _Numerische Mathematik_, to appear.
+% via Bezout resultants", _Numerische Mathematik_ 129 (2015),
+% 181--209.
 %%
 % [Trott 2007] M. Trott, "Applying GroebnerBasis to three problems in
 % geometry", _Mathematica in Education and Research_, 6 (1997), 15-28.
